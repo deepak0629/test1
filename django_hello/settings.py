eff7090b5f675bb9d656dd6b25a44be8b70aaf31
deepak <deepak06299@gmail.com>
2021-04-21 01:55:08 -0400

    temp1

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'hello'
 ]
 
@@ -119,4 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
